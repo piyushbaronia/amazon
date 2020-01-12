cc880d6ef2f0e9c1bdb9aabe67932a6c0ffc1ac8
root <piyush.ese@gmail.com>
2020-01-12 20:12:40 +0530

    Edited code

---
+++ Payment.java
@@ -0,0 +1,11 @@
+public class Payment
+{
+	public static void main(String args[])throws IOException
+	{
+		int i=10;
+		int j=20;
+		int add=i+j;
+		System.out.println("Addition : " + add);
+
+	}
+}
